1c721eee034124b4133d111b5eaea982f99694f3
Rajeev Bartwal <bartwalrajeev3@gmail.com>
2026-02-19 12:31:21 +0530

    C-001 Initial project setup

---
+++ src/main/java/com/acrevu/acrevu_backend/AcrevuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acrevu.acrevu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrevuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrevuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acrevu/acrevu_backend/AcrevuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrevu.acrevu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrevuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
